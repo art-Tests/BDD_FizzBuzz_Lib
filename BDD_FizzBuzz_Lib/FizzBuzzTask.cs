6d2edd27c88ee0c5fa9cbd1c826d16a859d37219
art <art.huang@ehsn.com.tw>
2018-09-10 17:34:02 +0800

    second green

--- BDD_FizzBuzz_Lib/FizzBuzzTask.cs
+++ BDD_FizzBuzz_Lib/FizzBuzzTask.cs
@@ -6,7 +6,19 @@ namespace BDD_FizzBuzz_Lib
     {
         public IList<string> GetResult(int length)
         {
-            return new[] { "1" };
+            var result = new string[length];
+            for (int i = 0; i < length; i++)
+            {
+                if ((i + 1) % 3 == 0)
+                {
+                    result[i] = "Fizz";
+                }
+                else
+                {
+                    result[i] = (i + 1).ToString();
+                }
+            }
+            return result;
         }
     }
 }

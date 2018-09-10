a4f3748310c9e4791307bccbfd5b7dcd065009f6
art <art.huang@ehsn.com.tw>
2018-09-10 17:18:36 +0800

    init

---
+++ BDD_FizzBuzz_Lib/FizzBuzzTask.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BDD_FizzBuzz_Lib
+{
+    public class FizzBuzzTask
+    {
+        public IList<string> GetResult(int length)
+        {
+            return new[] { "" };
+        }
+    }
+}
